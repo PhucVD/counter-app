6555f31e882ac5f292aeea0e9d555c27085fb4db
Phuc Vo <duyphuc_forza@yahoo.com>
2018-08-21 18:21:55 +0700

    Initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-sm btn-secondary">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    return this.state.count > 0 ? classes + "primary" : classes + "warning";
+  }
+}
+
+export default Counter;
